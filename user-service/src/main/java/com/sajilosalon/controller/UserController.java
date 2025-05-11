474c1deec74589accb1a53031b2a1b60f86ce462
Nischal <tamangnischal2018@gmail.com>
2025-05-11 18:56:46 +0545

    working on post and get method of user

--- user-service/src/main/java/com/sajilosalon/controller/UserController.java
+++ user-service/src/main/java/com/sajilosalon/controller/UserController.java
@@ -2,20 +2,29 @@ package com.sajilosalon.controller;
 
 
 import com.sajilosalon.modal.User;
+import com.sajilosalon.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+@PostMapping("/api/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
 
     @GetMapping("/api/users")
-    public User getUser() {
-        User user = new User();
-        user.setEmail("nischal@gmail.com");
-        user.setFullName("Nischal");
-        user.setPhone("+977 9818255542");
-        user.setRole("customer");
-        return user;
-    }
+    public List<User> getUser() {
+
+    return userRepository.findAll();
+}
 }
--- user-service/src/main/java/com/sajilosalon/repository/UserRepository.java
+++ user-service/src/main/java/com/sajilosalon/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.sajilosalon.repository;
 
-public class UserRepository {
+import com.sajilosalon.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
