b8fe08c550baf4e7c3beb20f9902ab9bc213f753
Nischal <tamangnischal2018@gmail.com>
2025-05-11 08:59:03 +0545

    created getter setter on modal

--- user-service/src/main/java/com/sajilosalon/modal/User.java
+++ user-service/src/main/java/com/sajilosalon/modal/User.java
@@ -1,4 +1,61 @@
 package com.sajilosalon.modal;
 
+import java.time.LocalDateTime;
+
 public class User {
+
+    private String fullName;
+private String email;
+private String phone;
+private String role;
+private LocalDateTime createdAt;
+private LocalDateTime updatedAt;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
