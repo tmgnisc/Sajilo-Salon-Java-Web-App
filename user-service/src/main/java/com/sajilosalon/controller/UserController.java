9fd04db2251f7cbf5073427342c06d9b80d428a0
Nischal <tamangnischal2018@gmail.com>
2025-05-11 19:12:20 +0545

    working on crud of user

--- user-service/src/main/java/com/sajilosalon/controller/UserController.java
+++ user-service/src/main/java/com/sajilosalon/controller/UserController.java
@@ -4,12 +4,10 @@ package com.sajilosalon.controller;
 import com.sajilosalon.modal.User;
 import com.sajilosalon.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -27,4 +25,29 @@ public class UserController {
 
     return userRepository.findAll();
 }
+
+
+@GetMapping("/api/users/{userId}")
+public User getUserById(@PathVariable("userId") Long id) throws Exception {
+    Optional<User> otp = userRepository.findById(id);
+    if (otp.isPresent()) {
+        return otp.get();
+    }
+    throw new Exception("user not found");
+}
+@PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
+
+    Optional<User> otp = userRepository.findById(id);
+    if (otp.isEmpty()) {
+        throw new Exception("user not found with id"+id);
+    }
+    User existingUser = otp.get();
+    existingUser.setFullName(user.getFullName());
+    existingUser.setEmail(user.getEmail());
+    existingUser.setPhone(user.getPhone());
+    existingUser.setRole(user.getRole());
+
+    return userRepository.save(existingUser);
+    }
 }
