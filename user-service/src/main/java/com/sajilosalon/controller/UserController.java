49c1d755f0ce24628bfc0130b2b5a96d53e3bf4b
Nischal <tamangnischal2018@gmail.com>
2025-05-13 16:41:48 +0545

    added lombok

--- user-service/src/main/java/com/sajilosalon/controller/UserController.java
+++ user-service/src/main/java/com/sajilosalon/controller/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-@PostMapping("/api/users")
+    @PostMapping("/api/users")
     public User createUser(@RequestBody @Valid User user) {
         return userRepository.save(user);
     }
@@ -24,43 +24,44 @@ public class UserController {
     @GetMapping("/api/users")
     public List<User> getUser() {
 
-    return userRepository.findAll();
-}
-
-
-@GetMapping("/api/users/{userId}")
-public User getUserById(@PathVariable("userId") Long id) throws Exception {
-    Optional<User> otp = userRepository.findById(id);
-    if (otp.isPresent()) {
-        return otp.get();
+        return userRepository.findAll();
     }
-    throw new Exception("user not found");
-}
-@PutMapping("/api/users/{id}")
+
+
+    @GetMapping("/api/users/{userId}")
+    public User getUserById(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> otp = userRepository.findById(id);
+        if (otp.isPresent()) {
+            return otp.get();
+        }
+        throw new Exception("user not found");
+    }
+
+    @PutMapping("/api/users/{id}")
     public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
 
-    Optional<User> otp = userRepository.findById(id);
-    if (otp.isEmpty()) {
-        throw new Exception("user not found with id"+id);
-    }
-    User existingUser = otp.get();
-    existingUser.setFullName(user.getFullName());
-    existingUser.setEmail(user.getEmail());
-    existingUser.setPhone(user.getPhone());
-    existingUser.setRole(user.getRole());
+        Optional<User> otp = userRepository.findById(id);
+        if (otp.isEmpty()) {
+            throw new Exception("user not found with id" + id);
+        }
+        User existingUser = otp.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setRole(user.getRole());
 
-    return userRepository.save(existingUser);
-    }
-
-
-@DeleteMapping("/api/users/{id}")
-    public String deleteUserById(@PathVariable Long id) throws  Exception {
-    Optional<User> otp = userRepository.findById(id);
-    if (otp.isEmpty()) {
-        throw new Exception("user not found with id"+id);
+        return userRepository.save(existingUser);
     }
 
-    userRepository.deleteById(otp.get().getId());
-return "User deleted";
-}
+
+    @DeleteMapping("/api/users/{id}")
+    public String deleteUserById(@PathVariable Long id) throws Exception {
+        Optional<User> otp = userRepository.findById(id);
+        if (otp.isEmpty()) {
+            throw new Exception("user not found with id" + id);
+        }
+
+        userRepository.deleteById(otp.get().getId());
+        return "User deleted";
+    }
 }
--- user-service/src/main/java/com/sajilosalon/modal/User.java
+++ user-service/src/main/java/com/sajilosalon/modal/User.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+
 @Entity
 public class User {
 
@@ -18,100 +19,25 @@ public class User {
     private Long id;
 
     private String fullName;
-@NotBlank(message = "username is mandatory")
+    @NotBlank(message = "username is mandatory")
     private String username;
 
     @NotBlank(message = "email is mandatory")
     @Email(message = "Email should be valid")
-private String email;
-private String phone;
+    private String email;
+    private String phone;
 
-@NotBlank(message = "Role is mandatory")
-private String role;
-
-
-@CreationTimestamp
-private LocalDateTime createdAt;
-@UpdateTimestamp
-private LocalDateTime updatedAt;
-
-@NotBlank(message = "password is mandatory")
-private String password;
+    @NotBlank(message = "Role is mandatory")
+    private String role;
 
 
-public User (){
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
-}
-    public User(String fullName, String email, String phone, String role, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.fullName = fullName;
-        this.email = email;
-        this.phone = phone;
-        this.role = role;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+    @NotBlank(message = "password is mandatory")
+    private String password;
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
