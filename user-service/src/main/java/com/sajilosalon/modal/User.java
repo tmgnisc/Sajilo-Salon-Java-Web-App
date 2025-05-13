6655ac9ab377138677ff089406aefbfa177b0cdc
Nischal <tamangnischal2018@gmail.com>
2025-05-13 12:55:58 +0545

    working on validation for creating user

--- user-service/src/main/java/com/sajilosalon/modal/User.java
+++ user-service/src/main/java/com/sajilosalon/modal/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
@@ -15,10 +16,15 @@ public class User {
     private Long id;
 
     private String fullName;
+@NotBlank(message = "username is mandatory")
+    private String username;
 
     @NotBlank(message = "email is mandatory")
+    @Email(message = "Email should be valid")
 private String email;
 private String phone;
+
+@NotBlank(message = "Role is mandatory")
 private String role;
 private LocalDateTime createdAt;
 private LocalDateTime updatedAt;
