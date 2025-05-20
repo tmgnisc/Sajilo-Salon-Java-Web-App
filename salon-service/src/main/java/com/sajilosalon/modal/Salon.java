78c30494f26bc21ae7a67830e10d8b844c4f4a4e
Nischal <tamangnischal2018@gmail.com>
2025-05-20 19:18:34 +0545

    created modal for salon service

---
+++ salon-service/src/main/java/com/sajilosalon/modal/Salon.java
@@ -0,0 +1,45 @@
+package com.sajilosalon.modal;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Salon {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalTime openTime;
+
+    @Column(nullable = false)
+    private LocalTime closeTime;
+}
