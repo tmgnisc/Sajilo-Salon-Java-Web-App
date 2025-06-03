834cbd7a53381963cac4e1c4bbbf66014fe51c71
Nischal <tamangnischal2018@gmail.com>
2025-06-03 09:07:46 +0545

    completed entity

--- service-offering/src/main/java/com/sajilosalon/modal/ServiceOffering.java
+++ service-offering/src/main/java/com/sajilosalon/modal/ServiceOffering.java
@@ -1,7 +1,32 @@
 package com.sajilosalon.modal;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class ServiceOffering {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+    @Column(nullable = false)
+    private Long categoryId;
+
+    private String image;
+
+
 }
