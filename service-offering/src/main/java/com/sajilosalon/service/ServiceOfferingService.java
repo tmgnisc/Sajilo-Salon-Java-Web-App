f7b0ee9c7e7d53d3745345035f4e6f86b569dfff
Nischal <tamangnischal2018@gmail.com>
2025-06-03 10:02:21 +0545

    created repository services and all

--- service-offering/src/main/java/com/sajilosalon/service/ServiceOfferingService.java
+++ service-offering/src/main/java/com/sajilosalon/service/ServiceOfferingService.java
@@ -11,8 +11,12 @@ import java.util.Set;
 public interface ServiceOfferingService {
 
     ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO);
-    ServiceOffering updateService(Long serviceId, ServiceOffering service);
-Set<ServiceOffering> getAllServiceBySalonId(Long salonId, Long categoryId);
-Set<ServiceOffering> getServicesByIds(Set<Long> ids);
 
+    ServiceOffering updateService(Long serviceId, ServiceOffering service) throws Exception;
+
+    Set<ServiceOffering> getAllServiceBySalonId(Long salonId, Long categoryId);
+
+    Set<ServiceOffering> getServicesByIds(Set<Long> ids);
+
+    ServiceOffering getServiceById(Long id) throws Exception;
 }
--- service-offering/src/main/java/com/sajilosalon/service/impl/ServiceOfferingServiceImpl.java
+++ service-offering/src/main/java/com/sajilosalon/service/impl/ServiceOfferingServiceImpl.java
@@ -4,30 +4,74 @@ import com.sajilosalon.dto.CategoryDTO;
 import com.sajilosalon.dto.SalonDTO;
 import com.sajilosalon.dto.ServiceDTO;
 import com.sajilosalon.modal.ServiceOffering;
+import com.sajilosalon.repository.ServiceOfferingRepository;
 import com.sajilosalon.service.ServiceOfferingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ServiceOfferingServiceImpl implements ServiceOfferingService {
+    private final ServiceOfferingRepository serviceOfferingRepository;
+
     @Override
     public ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO) {
-        return null;
+        ServiceOffering serviceOffering = new ServiceOffering();
+        serviceOffering.setImage(serviceDTO.getImage());
+        serviceOffering.setName(serviceDTO.getName());
+        serviceOffering.setSalonId(salonDTO.getId());
+        serviceOffering.setDescription(serviceDTO.getDescription());
+        serviceOffering.setCategoryId(categoryDTO.getId());
+        serviceOffering.setPrice(serviceDTO.getPrice());
+        serviceOffering.setDuration(serviceDTO.getDuration());
+        return serviceOfferingRepository.save(serviceOffering);
     }
 
     @Override
-    public ServiceOffering updateService(Long serviceId, ServiceOffering service) {
-        return null;
+    public ServiceOffering updateService(Long serviceId, ServiceOffering service) throws Exception {
+
+        ServiceOffering serviceOffering = serviceOfferingRepository.findById(serviceId).orElse(null);
+        if (serviceOffering == null) {
+            throw new Exception("service not exist with given id"+serviceId);
+        }
+        serviceOffering.setImage(service.getImage());
+        serviceOffering.setName(service.getName());
+
+        serviceOffering.setDescription(service.getDescription());
+
+        serviceOffering.setPrice(service.getPrice());
+        serviceOffering.setDuration(service.getDuration());
+
+        return serviceOfferingRepository.save(serviceOffering);
     }
 
     @Override
     public Set<ServiceOffering> getAllServiceBySalonId(Long salonId, Long categoryId) {
-        return Set.of();
+        Set<ServiceOffering> services= serviceOfferingRepository.findBySalonId(salonId);
+        if(categoryId!=null){
+            services=services.stream().filter((service)->service.getCategoryId()!=null && service.getCategoryId() == categoryId).collect(Collectors.toSet());
+        }
+        return services;
     }
 
     @Override
     public Set<ServiceOffering> getServicesByIds(Set<Long> ids) {
-        return Set.of();
+        List<ServiceOffering> services = serviceOfferingRepository.findAllById(ids);
+        return new HashSet<>(services);
+
+    }
+
+    @Override
+    public ServiceOffering getServiceById(Long id) throws Exception {
+        ServiceOffering serviceOffering = serviceOfferingRepository.findById(id).orElse(null);
+        if (serviceOffering == null) {
+            throw new Exception("service not exist with given id"+id);
+        }
+        return serviceOffering;
     }
 }
