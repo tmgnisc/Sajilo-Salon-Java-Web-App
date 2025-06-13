5908dad5f468289f06a08bd4dcc7e3adac3390f2
Nischal <tamangnischal2018@gmail.com>
2025-06-13 19:38:15 +0545

    created booking service impl

---
+++ booking-service/src/main/java/com/sajilosalon/service/impl/BookingServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sajilosalon.service.impl;
+
+import com.sajilosalon.domain.BookingStatus;
+import com.sajilosalon.dto.BookingRequest;
+import com.sajilosalon.dto.SalonDTO;
+import com.sajilosalon.dto.ServiceDTO;
+import com.sajilosalon.dto.UserDTO;
+import com.sajilosalon.modal.Booking;
+import com.sajilosalon.modal.SalonReport;
+import com.sajilosalon.service.BookingService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+public class BookingServiceImpl implements BookingService {
+    @Override
+    public Booking createBooking(BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByCustomer(Long customerId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Booking> getBookingsBySalon(Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public Booking getBookingById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Booking updateBooking(Long bookingId, BookingStatus status) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByDate(LocalDate date, Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public SalonReport getSalonReport(Long salonId) {
+        return null;
+    }
+}
