281d10120164677816ff23515eb9e538ae01a804
Nischal <tamangnischal2018@gmail.com>
2025-06-13 19:22:25 +0545

    working on booking service

---
+++ booking-service/src/main/java/com/sajilosalon/service/BookingService.java
@@ -0,0 +1,25 @@
+package com.sajilosalon.service;
+
+import com.sajilosalon.domain.BookingStatus;
+import com.sajilosalon.dto.BookingRequest;
+import com.sajilosalon.dto.SalonDTO;
+import com.sajilosalon.dto.ServiceDTO;
+import com.sajilosalon.dto.UserDTO;
+import com.sajilosalon.modal.Booking;
+
+import java.util.List;
+import java.util.Set;
+
+public interface BookingService {
+
+    Booking createBooking
+            (BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet);
+
+    List<Booking> getBookingsByCustomer(Long customerId);
+    List<Booking> getBookingsBySalon(Long salonId);
+    Booking getBookingById(Long id);
+    Booking updateBooking(Long bookingId, BookingStatus status);
+    
+
+
+}
