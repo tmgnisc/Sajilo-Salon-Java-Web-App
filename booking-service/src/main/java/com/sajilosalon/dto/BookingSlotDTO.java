0d241f230b936eaf72d1c87114c828b74e3f1b6f
Nischal <tamangnischal2018@gmail.com>
2025-06-16 12:51:10 +0545

    created separte slot dto

---
+++ booking-service/src/main/java/com/sajilosalon/dto/BookingSlotDTO.java
@@ -0,0 +1,11 @@
+package com.sajilosalon.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookingSlotDTO {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
